389e6cd494a5cfa94078134c1b02b82ff5f74fa8
scamjayi <scamjayi@0540274c-2ec4-11df-8a10-f33c597fd49f>
2010-04-20 04:40:17 +0000

    Se agrega la nube de etiquetas en la pagina de companies. Se agrego la libreria OpenClouds.

--- InfoCompany/src/ar/com/infocompany/actions/CompanyAction.java
+++ InfoCompany/src/ar/com/infocompany/actions/CompanyAction.java
@@ -2,6 +2,9 @@ package ar.com.infocompany.actions;
 
 import java.util.List;
 
+import org.mcavallo.opencloud.Cloud;
+import org.mcavallo.opencloud.Tag;
+
 import ar.com.infocompany.entities.Company;
 import ar.com.infocompany.services.IModelService;
 
@@ -13,8 +16,17 @@ public class CompanyAction extends ActionSupport{
 	private Company company = new Company();
 	private List<Company> companies;
 	private IModelService modelService;
+	private Cloud cloud;
 	
 	
+	public Cloud getCloud() {
+		return cloud;
+	}
+
+	public void setCloud(Cloud cloud) {
+		this.cloud = cloud;
+	}
+
 	public IModelService getModelService() {
 		return modelService;
 	}
@@ -25,6 +37,7 @@ public class CompanyAction extends ActionSupport{
 	
 	public String getAllCompanies() {
 		companies = modelService.getAllCompanies();
+		this.cloud = cloudTags();
 		return "success";
 	}
 	
@@ -63,5 +76,22 @@ public class CompanyAction extends ActionSupport{
 
 	public void setCompany(Company company) {
 		this.company = company;
+	}
+	
+	public Cloud cloudTags() {
+        /* Creación del objeto nube */
+        Cloud cloud = new Cloud();
+        /* Inserción de las tags en la nube */
+        int weith=35;
+        for (Company company : companies) {
+        	cloud.addTag(new Tag(company.getName(), company.getName(), weith - (weith/4)));
+        	weith=weith-(weith/4);
+        }
+        /* Establecimiento de propiedades de la nube  */
+        cloud.setMaxTagsToDisplay(15);
+        cloud.setMaxWeight(30);
+        cloud.setMinWeight(8);
+        /* Objeto creado  */
+        return cloud;
 	}
 }
